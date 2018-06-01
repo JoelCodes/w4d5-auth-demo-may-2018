fe2a54cd24fb3783e4df3c9ae13902d3d93d92d3
JoelCodes <amnshinness@yahoo.com>
2018-06-01 09:31:18 -0700

    Setup knex, added User table

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,12 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('name');
+    table.string('name').notNullable();
+    table.string('email').notNullable().unique();
+    table.string('password_hash').notNullable();
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex) {
   return knex.schema.dropTable('users');
 };
